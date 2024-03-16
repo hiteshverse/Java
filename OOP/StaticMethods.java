005dc415363c718a7d98af20a13a0eacd13ddb9d
Hitesh Kumar <hiteshkumar0778@gmail.com>
2024-03-16 22:09:38 +0530

    Static Blocks

--- OOP/StaticMethods.java
+++ OOP/StaticMethods.java
@@ -1,12 +1,12 @@
 public class StaticMethods {
-    public static void main(String[] args) {
-        greeting();
+    static int num;
+
+    // A static block can be called before the main method during the classloading
+    static {
+        System.out.println("Static Block Invoked" + StaticMethods.num);
     }
-
-
-    // AS you know you can't use any non-static stuff inside a static method without creating any instance of that class
-    // But you can make this non - static method to a static method you it can be used without creating any instance of the class
-    static void greeting() {
-        System.out.println("A Greeting Method is Called");
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
     }
 }
