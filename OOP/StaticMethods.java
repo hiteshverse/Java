3c7aceed7a5d2e8dc3a6af8a1306d63e467bb7b0
Hitesh Kumar <hiteshkumar0778@gmail.com>
2024-03-18 08:25:33 +0530

    File Updated

--- OOP/StaticMethods.java
+++ OOP/StaticMethods.java
@@ -1,12 +1,12 @@
 public class StaticMethods {
     static int num;
 
-    // A static block can be called before the main method during the classloading
     static {
-        System.out.println("Static Block Invoked : " + StaticMethods.num);
+        num = 5;
+        System.out.println("Static Block Invoked");
     }
+
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
+        System.out.println(StaticMethods.num);
     }
 }
